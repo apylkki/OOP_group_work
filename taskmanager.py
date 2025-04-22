5b0a0ff01b4ac7b18490827a8044ed02bf3f655a
saedekaskinen <157686625+saedekaskinen@users.noreply.github.com>
2025-04-22 17:11:39 +0300

    korjattu ongelma delete taskissa

--- taskmanager.py
+++ taskmanager.py
@@ -100,20 +100,22 @@ class TaskManager:
         if not self.tasks:
             print("No tasks available to delete.")
             return
-        
-        try:
-            task_id = int(input("Enter task ID to delete (or 0 to cancel): "))
-            if task_id == 0:
-                print("Delete operation canceled.")
+        while True:
+            try:
+                task_id = int(input("Enter task ID to delete (or 0 to cancel): "))
+                if task_id == 0:
+                    print("Delete operation canceled.")
+                    return
+                
+                if task_id in self.tasks:
+                    task = self.tasks[task_id]
+                    del self.tasks[task_id]
+                    print(f"Task deleted: {task.title}, {task.description}, {task.deadline}, {task.state}")
+                elif task_id not in self.tasks:
+                    print("Task not found. Please enter a valid task ID.")
+            except ValueError:
+                print("Invalid input. Please enter a valid task ID.")
                 return
-            
-            if task_id in self.tasks:
-                task = self.tasks[task_id]
-                del self.tasks[task_id]
-                print(f"Task deleted: {task.title}, {task.description}, {task.deadline}, {task.state}")
-        except ValueError:
-            print("Invalid input. Please enter a valid task ID.")
-            return
                 
 
     def list_of_tasks(self):
